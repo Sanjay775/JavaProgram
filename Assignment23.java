ee5fe5ae779deea72770b82f7106df23956018bc
Sanjay775 <sanjaysiddhu775@gmail.com>
2024-10-09 14:45:37 +0530

    ass23

---
+++ Assignment23.java
@@ -0,0 +1,56 @@
+package java_online;
+class grantparent
+{
+	 void method1()
+	 {
+	 }
+	 void method2()
+	 {
+	 }
+	 }
+abstract class father extends grantparent
+{
+	 abstract void method3();
+	 abstract void method4();
+	 void method5()
+	 {
+	 }
+	 void method6()
+	 {
+	 }
+	 }
+
+
+public class Assignment23 extends father{
+	void method7()
+	{
+	}
+	void method8()
+	{
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Assignment23 as=new Assignment23();
+		as.method1();
+		as.method2();
+		as.method3();
+		as.method4();
+		as.method5();
+		as.method6();
+		as.method7();
+		as.method8();
+		
+
+	}
+	@Override
+	void method3() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	void method4() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
