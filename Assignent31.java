a26c80cc3c318f290f3a8d6d861242381ef2c133
Sanjay775 <sanjaysiddhu775@gmail.com>
2024-10-09 14:35:11 +0530

    assignment31

---
+++ Assignent31.java
@@ -0,0 +1,20 @@
+package java_online;
+
+import java.util.Arrays;
+
+public class Assignent31 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String []a= new String[4];
+		a[0]="sanju";
+		a[1]="ranju";
+		a[2]="manju";
+		a[3]= null;
+		
+		System.out.println(Arrays.toString(a));
+		
+
+	}
+
+}
